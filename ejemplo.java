d7a20f71da2d259f3df3fc56b59c3aa1208a4c12
Delia <deli_delia19@hotmail.es>
2021-10-21 13:37:43 +0200

    Modificado ejemplo

--- ejemplo.java
+++ ejemplo.java
@@ -1,6 +1,6 @@
 public class ejemplo{
 	public static void main(String[]args){
-		System.out.println("Escribe una cadena: ");
+		System.out.println("Escribe la cadena: ");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("La cadena es: " + sc.nextLine());  
 	
